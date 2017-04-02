184b495e6256a3a6934b56bcb0c9a6674c9b925d
John Daly <jpdaly@gmail.com>
2017-04-02 16:57:44 +0100

    some spotify gubbins

--- public/app.js
+++ public/app.js
@@ -1,8 +1,24 @@
+var config = require('./config');
 var Twit = require('twit');
-var config = require('config');
+var SpotifyWebApi = require('spotify-web-api-node');
 
 var T = new Twit(config);	
-	
+
+var spotify = new SpotifyWebApi();
+
+// get a list of elvis albums
+spotify.getArtistAlbums('43ZHCT0cAZBISjO8DG9PnE')
+  .then(function(data) {
+    console.log('Artist albums', data.body);
+  }, function(err) {
+    console.error(err);
+  });
+
+var setNewTrack = 
+
+var random = Math.floor((Math.random() * allElvis.length) + 1);
+var thisHoursElvis = allElvis[random];
+console.log(thisHoursElvis)
 
 var app = function(){
 
