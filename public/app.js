3084979327cc6e1b6a8ae3d76eef5bd197ae87aa
John Daly <jpdaly@gmail.com>
2017-04-02 20:00:54 +0100

    try an alternative route

--- public/app.js
+++ public/app.js
@@ -5,37 +5,54 @@ var SpotifyWebApi = require('spotify-web-api-node');
 var T = new Twit(config);	
 
 var spotify = new SpotifyWebApi();
-var randomAlbum = null;
+
+
+var makeRequest = function(url, callback){
+  var request = new XMLHttpRequest();
+  request.open("GET", url);
+  request.onload = callback;
+  request.send();
+}
+
+var handleWeatherRequest = function(){
+
+  if(this.status !== 200){
+    return;
+  }
+
+  var jsonString = this.responseText;
+  var returnedObject = JSON.parse(jsonString);
+  console.log(returnedObject);
+
+
+}
 
 
 // get a list of elvis albums
-var getAllAlbums = function(){
-  spotify.getArtistAlbums('43ZHCT0cAZBISjO8DG9PnE')
-  .then(function(data) {
-    getRandomAlbum(data.body.index);
-  }, function(err) {
+var getAllAlbums = function(callback){
+  spotify.getArtistAlbums('43ZHCT0cAZBISjO8DG9PnE'
+  .then(data, callback(getRandomAlbum))
+  .then(getAlbumTracks(data))
+  , function(err) {
     console.error(err);
-  });
-
-
+  };
 }
 
-var getRandomAlbum = function(data){
-  var random = Math.floor((Math.random() * data.length) + 1);
-  var album = data[random];
+var getRandomAlbum = function(arrayOfAlbums, callback){
+  var random = Math.floor((Math.random() * arrayOfAlbums.length) + 1);
+  return arrayOfAlbums[random].id;
 }
 
-var getAlbumTracks = function(albumId){
+var getAlbumTracks = function(albumId, callback){
   spotify.getAlbum(albumId);
 }
 
 
 
 var app = function(){
-  var all = getAllAlbums(getRandomAlbum);
-
-
+  var url = 'https://api.spotify.com/v1/artists/43ZHCT0cAZBISjO8DG9PnE/albums';
+  makeRequest(url, handleRequest);
 
 }
 
-app();
+window.onload = app;
