67f094e8fdfeab9908b320ccad6a4ca59e52867e
John Daly <jpdaly@gmail.com>
2017-09-17 13:49:58 +0100

    fix issue with spotify auth

--- public/es6app.js
+++ public/es6app.js
@@ -12,79 +12,72 @@ require('isomorphic-fetch');
 
 //get list of Super Furry albums
 const getTrack = () => {
-				
 
-				spotify.clientCredentialsGrant()
-								.then(data => {
-												console.log("The access token expires in " + data.body['expires_in']);
-												console.log("The access token is " + data.body['access_token']);
-												spotify.setAccessToken(data.body['access_token']);
-								}, err => {
-												console.log("error getting token" + err.message)
-								}
-								)
 
-				// console.log(spotifyAccessToken);
-				// spotify.setAccessToken(spotifyAccessToken);
+	spotify.clientCredentialsGrant()
+	.then(data => {
+		console.log("The access token expires in " + data.body['expires_in']);
+		console.log("The access token is " + data.body['access_token']);
+		spotify.setAccessToken(data.body['access_token']);
+		console.log(spotify);
+		console.log(data.body)
 
-.then(
-				spotify.getArtistAlbums('0FOcXqJgJ1oq9XfzYTDZmZ', {limit:50, album_type:"album,single"})
-								.then(data => {
-												console.log(data.body);
-												return data.body;
-								}, err => {
-												console.error(err);
-								})
-				// fetch('https://api.spotify.com/v1/artists/0FOcXqJgJ1oq9XfzYTDZmZ/albums?album_type=album&limit=35')
-				// .then(response => {
-				//         if(response.status != 200){
-				//             console.error('Album fetch error:', response.status, response.statusText);
-				//         }
-				// return response.json();
-				// })
+
+	spotify.getArtistAlbums('0FOcXqJgJ1oq9XfzYTDZmZ', {limit:50, album_type:"album,single"})
+	.then(data => {
+		console.log(data.body);
+		return data.body;
+	}, err => {
+		console.error(err);
+	})
+
 
 				.then(data => {
-								var array = data.items;
+					var array = data.items;
 
-								var random = Math.floor(Math.random() * array.length);
-								const album = array[random];
-								console.log('selected album:', album.name);
-								return album.id;
+					var random = Math.floor(Math.random() * array.length);
+					const album = array[random];
+					console.log('selected album:', album.name);
+					return album.id;
 				})
 				.catch(response => {
-								console.log('spotify album getter error:', response);
+					console.log('spotify album getter error:', response);
 				})
 				.then(id => {
 								//get that album
 								spotify.getAlbumTracks(id, {limit: 30})
-												.then(data => {
+								.then(data => {
 																// choose a random song from it
 																var tracks = data.body.items;
 																var random = Math.floor(Math.random() * tracks.length);
 																var randomTrack = tracks[random];
 																console.log('track:', randomTrack.name);
 																return randomTrack;
-												})
+															})
 
 								.then(track => {
-												T.post('statuses/update', {status: "Tick, tock, it's Furry O'Clock...\n" + track.external_urls.spotify},
-																				(err, data, response) => {
-																								switch(response){
-																												case 187:
-																																console.log('Code 187. Duplicate tweet!')
-																																				break;
-																												case 200:
-																																console.log('Successfully posted!');
-																																break;
-																												default:
-																																console.log(err);
+									T.post('statuses/update', {status: "Tick, tock, it's Furry O'Clock...\n" + track.external_urls.spotify},
+										(err, data, response) => {
+											switch(response){
+												case 187:
+												console.log('Code 187. Duplicate tweet!')
+												break;
+												case 200:
+												console.log('Successfully posted!');
+												break;
+												default:
+												console.log(err);
 
-																								}
-																				})
+											}
+										})
+								}, err => {
+									console.log(err);
 								})
-				}))
+							})
+				});
+			
 }
 
 
-// getCredentials();
+// // getCredentials();
 getTrack();
